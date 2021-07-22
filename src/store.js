ae38f8842c6feb4af49da2f7e7afbce40da3afa4
Mertcan <mertcan.segmen@hotmail.com>
2021-07-22 18:47:09 +0300

    added language slice to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./slices/categorySlice";
+import languageSlice from "./slices/languageSlice";
 
 export const store = configureStore({
     reducer: {
         categories: categorySlice,
+        language: languageSlice,
     },
 });
