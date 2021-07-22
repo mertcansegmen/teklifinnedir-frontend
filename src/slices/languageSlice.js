78c02c2ed66006e13c93e8ccea6e05d8543749cf
Mertcan <mertcan.segmen@hotmail.com>
2021-07-22 22:13:43 +0300

    bugfixes on language slice

--- src/slices/languageSlice.js
+++ src/slices/languageSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import i18n from "i18next";
 
 const language = localStorage.getItem("language")
-    ? JSON.parse(localStorage.getItem("language"))
+    ? localStorage.getItem("language")
     : null;
 
-const initialState = language;
+const initialState = { language };
 
 export const languageSlice = createSlice({
     name: "location",
@@ -14,20 +15,24 @@ export const languageSlice = createSlice({
             let language =
                 state.language || navigator.language || navigator.userLanguage;
 
-            if (language !== "en-US" || language !== "tr-TR") {
+            if (language !== "en-US" && language !== "tr-TR") {
                 language = "en-US";
             }
+
+            i18n.changeLanguage(language);
 
             state.language = language;
         },
         setLanguage: (state, { payload }) => {
             let language = payload;
 
-            if (language !== "en-US" || language !== "tr-TR") {
+            if (language !== "en-US" && language !== "tr-TR") {
                 language = "en-US";
             }
 
             localStorage.setItem("language", language);
+
+            i18n.changeLanguage(language);
 
             state.language = language;
         },
