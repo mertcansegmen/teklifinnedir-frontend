2a5e3e1b9d6170cb093d1180637bd5163aafbc3b
Mertcan <mertcan.segmen@hotmail.com>
2021-07-23 19:33:20 +0300

    organized the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,57 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import React from "react";
+import { Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import SearchBar from "./SearchBar";
+import MessagesNavLink from "./MessagesNavLink";
+import UserNavDropdown from "./UserNavDropdown";
+import LoginSignUpNavLink from "./LoginSignUpNavLink";
+import { NonUserNavDropdown } from "./NonUserNavDropdown";
 
 const Header = () => {
+    const dispatch = useDispatch();
 
-  return (
-    <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>GetGo</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            {/* <Route render={({ history }) => <SearchBox history={history} />} /> */}
-            <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link>
-                  <i className='fas fa-shopping-cart'></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              {/* {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <i className='fas fa-user'></i> Sign In
-                  </Nav.Link>
-                </LinkContainer>
-              )} */}
-              {/* {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <LinkContainer to='/admin/userlist'>
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/productlist'>
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/orderlist'>
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )} */}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  )
-}
+    const { user } = useSelector((state) => state.user) || {};
 
-export default Header
+    return (
+        <header>
+            <Navbar
+                bg="dark"
+                variant="dark"
+                expand="lg"
+                collapseOnSelect
+                className="py-3"
+            >
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>GetGo</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Route
+                            render={({ history }) => (
+                                <SearchBar history={history} />
+                            )}
+                        />
+                        <Nav>
+                            {user ? (
+                                <>
+                                    <MessagesNavLink />
+                                    <UserNavDropdown user={user} />
+                                </>
+                            ) : (
+                                <>
+                                    <LoginSignUpNavLink />
+                                    <NonUserNavDropdown />
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
