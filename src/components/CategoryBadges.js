920314e7809d3e41a60cd62429ca17e250d1b4bc
Mertcan <mertcan.segmen@hotmail.com>
2021-07-25 05:57:19 +0300

    moved category badges into a seperate component

---
+++ src/components/CategoryBadges.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BadgeCarousel from "../components/BadgeCarousel";
+import { getCategories } from "../slices/categorySlice";
+import { useTranslation } from "react-i18next";
+import Loader from "./Loader";
+
+const CategoryBadges = ({ className }) => {
+    const { t } = useTranslation();
+
+    const dispatch = useDispatch();
+
+    const { categories, loading, error } = useSelector(
+        (state) => state.categories
+    );
+
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch]);
+
+    if (loading)
+        return (
+            <div className={className}>
+                <Loader />
+            </div>
+        );
+
+    if (error)
+        return (
+            <div className={className}>
+                <h1>{error}</h1>
+            </div>
+        );
+
+    return (
+        <div className={className}>
+            <h3>{t("categories")}</h3>
+
+            <BadgeCarousel
+                className="mt-4"
+                chipInfoList={categories.map((category) => {
+                    return {
+                        id: category.id,
+                        title: category.name,
+                        colorCode: category.colorCode,
+                        faIconName: category.faIconName,
+                        link: `search?category=${category.id}`,
+                    };
+                })}
+            />
+        </div>
+    );
+};
+
+export default CategoryBadges;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,42 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Spinner } from "react-bootstrap";
-import BadgeCarousel from "../components/BadgeCarousel";
-import { getCategories } from "../slices/categorySlice";
-import { useTranslation } from "react-i18next";
+import React from "react";
+import CategoryBadges from "../components/CategoryBadges";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const HomeScreen = ({ match }) => {
-    const { t } = useTranslation();
-
-    const dispatch = useDispatch();
-
-    const { categories, loading, error } = useSelector(
-        (state) => state.categories
-    );
-
-    useEffect(() => {
-        dispatch(getCategories());
-    }, [dispatch]);
-
-    if (loading) return <Spinner animation="border" />;
-
-    if (error) return <h1>{error}</h1>;
-
     return (
         <>
-            <h3 className="mt-4">{t("categories")}</h3>
-            <BadgeCarousel
-                className="mt-3"
-                chipInfoList={categories.map((category) => {
-                    return {
-                        id: category.id,
-                        title: category.name,
-                        colorCode: category.colorCode,
-                        faIconName: category.faIconName,
-                        link: `search?category=${category.id}`,
-                    };
-                })}
-            />
+            <CategoryBadges className="mt-4" />
+
+            <FeaturedProducts className="mt-5" />
         </>
     );
 };
