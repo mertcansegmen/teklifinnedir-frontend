20547033cf59306aa0d331e999b7d3bfa12c8603
Mertcan Seğmen <mertcan.segmen@hotmail.com>
2022-08-31 17:13:11 +0300

    added AlertError component

---
+++ src/components/Errors/AlertError.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Alert, Button } from "antd";
+
+const AlertError = ({
+    message,
+    description,
+    showRetryButton,
+    onRetryButtonClick,
+    ...props
+}) => {
+    if (!showRetryButton && onRetryButtonClick) {
+        throw new Error(
+            "showRetryButton must be true if onRetryButtonClick is defined"
+        );
+    }
+
+    return (
+        <Alert
+            message={message}
+            type="error"
+            showIcon
+            action={
+                <>
+                    {showRetryButton && (
+                        <Button
+                            size="small"
+                            type="primary"
+                            onClick={(e) =>
+                                onRetryButtonClick && onRetryButtonClick(e)
+                            }
+                        >
+                            Retry
+                        </Button>
+                    )}
+                </>
+            }
+            {...props}
+        />
+    );
+};
+
+export default AlertError;
