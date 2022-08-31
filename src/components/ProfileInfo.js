a071c2048c8ede7800dcf4bd28a8b1390d90e36b
Mertcan Seğmen <mertcan.segmen@hotmail.com>
2022-09-01 01:37:22 +0300

    added error and skeleton loading to ProfileInfo component

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,14 +1,46 @@
 import React from "react";
+
 import Rating from "./Rating";
+import ProfileInfoSL from "./SkeletonLoaders/ProfileInfoSL";
+import AlertError from "./Errors/AlertError";
 
 const ProfileInfo = ({
     firstName = "",
     lastName = "",
     rating = 0,
     profileImage = require("../assets/images/profile-placeholder.png"),
+    loading,
+    error,
+    showRetryButton,
+    onRetryButtonClick,
+    ...props
 }) => {
+    if (!showRetryButton && onRetryButtonClick) {
+        throw new Error(
+            "showRetryButton must be true if onRetryButtonClick is defined."
+        );
+    }
+
+    if (error) {
+        return (
+            <AlertError
+                message={error}
+                showRetryButton={showRetryButton}
+                onRetryButtonClick={onRetryButtonClick}
+                {...props}
+            />
+        );
+    }
+
+    if (loading) {
+        return <ProfileInfoSL {...props} />;
+    }
+
     return (
-        <div className="d-flex flex-row align-items-center mt-5">
+        <div
+            {...props}
+            className={`d-flex flex-row align-items-center ${props.className}`}
+        >
             <img
                 style={{ width: "150px", height: "150px" }}
                 className="rounded-circle"
